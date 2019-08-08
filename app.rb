809e384ca8d306c777fc73d60cbe6ff3efdc0c86
github username <github email address>
2019-08-07 17:55:48 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require './environment'
-
+require 'pry'
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+      # binding.pry
+      @pirate = Pirate.new(params[:pirate][:name],params[:pirate][:weight],params[:pirate][:weight])
+      @ship1 = Ship.new(params[:pirate][:ships][0][:name],params[:pirate][:ships][0][:type],params[:pirate][:ships][0][:booty])
+      @ship2 = Ship.new(params[:pirate][:ships][1][:name],params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+      
+      erb :'/pirates/show'
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,8 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    def initialize(name,weight,height)
+        @name = name
+        @weight = weight
+        @height = height
+    end
 end
